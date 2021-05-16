aa8185a2e4117af418050d6603e9fdf8129cf841
serge <serge@localhost>
2021-05-16 10:40:16 +0200

    modif revision

--- 9_divers/95_revision_list_dict_tuple_funct/95_revision.py
+++ 9_divers/95_revision_list_dict_tuple_funct/95_revision.py
@@ -6,9 +6,32 @@ ma_var = "bien"
 def essai():
     pass
 
-liste = [2,71828, abs(-9), essai, 0, 1]
+liste = [2.71828, abs(-9), essai, 0, 1]
+
+print("liste[2]", liste[2])
+print("liste[-2]", liste[-2])
 
 liste.append(14)
 liste.extend(("127.0.0.1", 8000))
 
+print()
 print("liste", liste)
+
+print()
+for item in liste:
+    print(item)
+
+print()
+for i in range(len(liste)):
+    print(liste[i])
+
+print()
+for item in liste:
+    i = liste.index(item)
+    print(liste[i])
+
+dico = {"nom": "CAMUS", "prénom": "ALBERT", "profession": "auteur à succès"}
+
+print()
+for key, val in dico.items():
+    print("clé", key, "valeur", val)
