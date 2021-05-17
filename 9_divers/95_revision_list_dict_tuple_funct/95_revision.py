c7b7fb06c988c7a9edd3490ec1b6c74fdd98419f
serge <serge@localhost>
2021-05-17 15:38:19 +0200

    mieux

--- 9_divers/95_revision_list_dict_tuple_funct/95_revision.py
+++ 9_divers/95_revision_list_dict_tuple_funct/95_revision.py
@@ -14,25 +14,25 @@ print("liste[-2]", liste[-2])
 liste.append(14)
 liste.extend(("127.0.0.1", 8000))
 
-print("1")
+print("\n-----------------------------------------1")
 print("liste", liste)
 
-print("2")
+print("\n-----------------------------------------2")
 for item in liste:
     print(item)
 
-print("3")
+print("\n-----------------------------------------3")
 for i in range(len(liste)):
     print(liste[i])
 
-print("4")
+print("\n-----------------------------------------4")
 for item in liste:
     i = liste.index(item)
     print(liste[i])
 
 dico = {"nom": "CAMUS", "prénom": "ALBERT", "profession": "auteur à succès"}
 
-print("5")
+print("\n-----------------------------------------5")
 for key, val in dico.items():
     print("clé", key, "valeur", val)
 
