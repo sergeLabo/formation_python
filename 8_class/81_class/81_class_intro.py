63824d7205f51d9b2a13909ed4f8a99417c26ab1
serge <serge@localhost>
2021-06-02 18:44:13 +0200

    correction class

--- 8_class/81_class/81_class_intro.py
+++ 8_class/81_class/81_class_intro.py
@@ -1,8 +1,4 @@
 
-# print("\ndir(str)\n", dir(s), "\n"*3)
-# s = s.encode()
-# print(type(s), s, "\n"*3)
-
 
 ### Construire une  class
 class Subliminale:
@@ -21,10 +17,6 @@ m = Minimale()
 print("m", m)
 print("\n\n")
 
-# Pourquoi __name__
-print("Pourquoi __name__?")
-print(dir())
-print("__name__ =", __name__)
 
 
 class MyTest:
@@ -40,25 +32,32 @@ class Bicycle:
     def __init__(self, color, kind):
         """C'est le constructeur"""
 
-        self.color = color
-        # self.color = col
+        # self.color = color
+        self.col = color
+
         self.kind = kind
-        a = self.pierre_soulages()
-        print("pierre soulages =", a)
+
+        # a = self.pierre_soulages()
+        # print("pierre soulages =", a)
 
     def pierre_soulages(self):
-        self.color = "noir"
+        self.col = "noir"
 
 
 def pierre_soulages(new_color):
     return new_color
 
-c = pierre_soulages("rose")
-print("pierre_soulages(new_color):", c)
 
 my_bicycle = Bicycle("rouge", "vtt")
-print(f"la couleur de mon velo rouge est {my_bicycle.color}")
+print(f"la couleur de mon velo rouge est {my_bicycle.col}")
 print(f"il est du type {my_bicycle.kind}")
+
+# my_bicycle.pierre_soulages()
+# print("la couleur de mon velo rouge est", my_bicycle.col")
+
+# c = pierre_soulages("rose")
+# print("pierre_soulages(new_color):", c)
+
 
 
 class Contact:
@@ -76,10 +75,14 @@ class Contact:
 c = Contact("toto")
 print(f"Le numéro de toto est {c.get_phone()}")
 
-copains = ["Emmanuel", "Jean"]
-mon_carnet = {}
-for copain in copains:
-    mon_carnet[copain] = Contact(copain)
-mon_carnet['Emmanuel'].set_phone("djembé")
+# copains = ["Emmanuel", "Jean"]
+# mon_carnet = {}
+# for copain in copains:
+    # mon_carnet[copain] = Contact(copain)
+# print("mon_carnet", mon_carnet)
 
-print(f"Le numéro de Emmanuel est {mon_carnet['Emmanuel'].get_phone()}")
+# print(mon_carnet["Jean"])
+
+# mon_carnet['Emmanuel'].set_phone("djembé")
+
+# print("Le numéro de Emmanuel est", mon_carnet['Emmanuel'].get_phone())
---
+++ 8_class/81_class/82_class_explications.py
@@ -0,0 +1,39 @@
+
+from time import sleep
+from random import randint
+
+class Skeleton:
+
+    def __init__(self, nb_points, nb_coord):
+
+        self.nb_coord = nb_coord
+        self.nb_points = nb_points
+        self.points = None
+        self.average = 0
+
+    def update_average(self):
+        if self.nb_points == 1 and self.nb_coord == 3:
+            self.average = (points[0][0] + points[0][1] + points[0][2])/3
+        else:
+            self.average = None
+
+    def add_points(self, points):
+        # Ajout des points
+        self.points = points
+
+        # Mise à jour de la moyenne
+        self.update_average()
+
+
+if __name__ == '__main__':
+
+    skelet = Skeleton(1, 3)
+    print(skelet.average)
+    print(dir(skelet))
+
+    while 1:
+        points = [[randint(50, 100), randint(0, 100), randint(-50, 50)]]
+        skelet.add_points(points)
+        moyenne = skelet.average
+        print("moyenne", moyenne)
+        sleep(1)
